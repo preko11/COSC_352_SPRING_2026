e71a810068aa3e2a1afa05c868bd166e3378a608
cohaw1 <cohaw1@morgan.edu>
2026-02-27 22:13:08 -0500

    Update run.sh

--- corey_hawkins/project05/run.sh
+++ corey_hawkins/project05/run.sh
@@ -1,1 +1,27 @@
+# !/bin/bash
 
+# defines image name and exits immediately if a non-zero status exists
+
+set -e
+
+IMAGE_NAME="homicide-histogram-app"
+
+CONTAINER_NAME="homicide-app-runner"
+
+echo " Building Docker image: $IMAGE_NAME "
+
+# building the Docker image
+
+docker build -t $IMAGE_NAME .
+
+echo ""
+
+echo " Running Docker container: $CONTAINER_NAME "
+
+# runs the Docker container, while —rm allows interactive terminal and attaches STDOUT/STDER
+
+docker run --rm --name $CONTAINER_NAME $IMAGE_NAME
+
+echo ""
+
+echo " Script execution finished "
